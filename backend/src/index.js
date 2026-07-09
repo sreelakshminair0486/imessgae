3b6eb1c9fd295fc930abd5b2d58c5f87569f9d2c
sreelakshminair0486 <nairlakshmi0486@gmail.com>
2026-07-09 16:59:30 +0530

    Add user and message models, connect to MongoDB, and set up server health check

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,24 @@
 //const express =require("express")
 import express from "express";
+import User from "./models/user.model.js";
+import connectToMongoDB from "./lib/db.js";
+import {clrekMiddleware} from '@clerk/express'
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app=express();
 
-app.listen(3000,()=>console.log("Server is listening 3000 "));
+const PORT=process.env.PORT;
+const PORT=process.env.FRONTEND_URL;
+
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL,credentials:true}));
+app.use(clrekMiddleware());
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({ok:true});
+});
+
+app.listen(PORT,()=> {connectToMongoDB(); console.log("Server is listening 3000 ")});
 
