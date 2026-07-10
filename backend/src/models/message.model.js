acb45d934194a77ea6b3280129dc5189b76861fb
sreelakshminair0486 <nairlakshmi0486@gmail.com>
2026-07-10 20:09:24 +0530

    refactor: update message and user schemas for improved structure and clarity

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-	{
-		senderId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		receiverId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		message: {
-			type: String,
-			required: true,
-		},
-		// createdAt, updatedAt
-	},
-	{ timestamps: true }
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    video: {
+      type: String,
+    },
+  },
+  { timestamps: true },
 );
 
 const Message = mongoose.model("Message", messageSchema);
