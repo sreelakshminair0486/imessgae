7da2793a9104f0f143df890395dc7e130ba50b91
sreelakshminair0486 <nairlakshmi0486@gmail.com>
2026-07-10 10:42:59 +0530

    Add Dockerfile and .dockerignore; update backend package.json and index.js for static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,10 @@
 import express from "express";
 import User from "./models/user.model.js";
 import connectToMongoDB from "./lib/db.js";
-import {clrekMiddleware} from '@clerk/express'
+import {clerkMiddleware} from '@clerk/express'
+import fs from "fs";
+import path from "path";
+
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,15 +13,25 @@ dotenv.config();
 const app=express();
 
 const PORT=process.env.PORT;
-const PORT=process.env.FRONTEND_URL;
+const FRONTEND_URL=process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
+
 
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL,credentials:true}));
-app.use(clrekMiddleware());
+app.use(clerkMiddleware());
 
 app.get("/health",(req,res)=>{
     res.status(200).json({ok:true});
 });
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT,()=> {connectToMongoDB(); console.log("Server is listening 3000 ")});
 
